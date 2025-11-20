600d50434e819865403deab66363cab9ea9b9e01
Himanshu Kushwaha <Himanshukushwaha548@gmail.com>
2025-11-20 18:09:41 +0530

    Add changeBG function and update file list
    Added 03_myScript.js with a changeBG function to modify background and heading. Updated index.html to include 03_myScript.js in the file list.

---
+++ 03_myScript.js
@@ -0,0 +1,5 @@
+function changeBG(){
+        document.body.style.backgroundColor = 'red';
+        document.querySelector('h1').innerText = "MOGAMBO KHUSH HUA!!"
+        document.querySelector('h1').style.color = 'white';
+}
